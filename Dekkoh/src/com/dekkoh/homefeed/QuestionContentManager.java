5f2e0590162cc8babc6985f02459468fe6a4142d
hobbes09 <sourin.sutradhar@gmail.com>
2015-01-06 21:15:33 +0530

    Implementing runnable interface in QuestionContentManager

--- Dekkoh/src/com/dekkoh/homefeed/QuestionContentManager.java
+++ Dekkoh/src/com/dekkoh/homefeed/QuestionContentManager.java
@@ -6,23 +6,32 @@ import com.dekkoh.application.ApplicationState;
 import com.dekkoh.datamodel.Question;
 import com.dekkoh.service.APIProcessor;
 import com.dekkoh.util.FileManager;
+import com.dekkoh.util.Log;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
-public class QuestionContentManager {
+public class QuestionContentManager implements Runnable{
+	
+	public static boolean updateSuccessful = false;
 
 	public static void fetchQuestionsFromBackend() throws Exception{
 		List<Question> newQuestionList = APIProcessor.getQuestions((Activity)HomeScreen.homeScreenContext, ApplicationState.getHomefeedQuestion_Offset(), ApplicationState.getHomefeedQuestion_Limit(), 0, 0, null, null, 0, null, false, null, null, null);
 		List<Question> existingQuestionList;
-		ApplicationState.updateOffset();
-		if(FileManager.getInstance().isObjectFileExistsInExternalStorage((Activity)HomeScreen.homeScreenContext, ApplicationState.getQuestionsfile())){
-			existingQuestionList = (List<Question>)FileManager.getInstance().readObjectFileFromExternalStorage((Activity)HomeScreen.homeScreenContext, ApplicationState.getQuestionsfile());
-			existingQuestionList.addAll(newQuestionList);
-			FileManager.getInstance().writeObjectFileInExternalStorage((Activity)HomeScreen.homeScreenContext, ApplicationState.getQuestionsfile(), existingQuestionList);
-		}else{
-			FileManager.getInstance().writeObjectFileInExternalStorage((Activity)HomeScreen.homeScreenContext, ApplicationState.getQuestionsfile(), newQuestionList);
+		if(newQuestionList != null){
+			ApplicationState.updateOffset();
+			if(FileManager.getInstance().isObjectFileExistsInExternalStorage((Activity)HomeScreen.homeScreenContext, ApplicationState.getQuestionsfile())){
+				existingQuestionList = (List<Question>)FileManager.getInstance().readObjectFileFromExternalStorage((Activity)HomeScreen.homeScreenContext, ApplicationState.getQuestionsfile());
+				existingQuestionList.addAll(newQuestionList);
+				FileManager.getInstance().writeObjectFileInExternalStorage((Activity)HomeScreen.homeScreenContext, ApplicationState.getQuestionsfile(), existingQuestionList);
+			}else{
+				FileManager.getInstance().writeObjectFileInExternalStorage((Activity)HomeScreen.homeScreenContext, ApplicationState.getQuestionsfile(), newQuestionList);
+			}
+			updateSuccessful = true;
 		}
+		Log.e("Can't fetch question from Backend");
+		updateSuccessful = false;
 	}
 	
 	public static Question getQuestionFromExternalStorage() throws Exception{
@@ -33,7 +42,9 @@ public class QuestionContentManager {
 			question = existingQuestionList.get(ApplicationState.getHomefeedQuestion_CurrentIndex());
 			return question;
 		}else{
-			fetchQuestionsFromBackend();
+			Thread threadQuestionUpdater = new Thread("Question Updater");
+			threadQuestionUpdater.start();
+			threadQuestionUpdater.join();
 			return getQuestionFromExternalStorage();
 		}	
 	}
@@ -57,4 +68,15 @@ public class QuestionContentManager {
 		questionFragArgs.putString("QUESTION", question.getQuestion());
 		return questionFragArgs;
 	}
+
+	@Override
+	public void run() {
+		try {
+			fetchQuestionsFromBackend();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}
+
 }
+	
