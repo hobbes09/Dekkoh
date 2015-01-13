f7222c3e673992d6f0b79de48e2b4baffe67f901
hobbes09 <sourin.sutradhar@gmail.com>
2015-01-13 23:41:03 +0530

    Fetching 20 questions from backend

--- Dekkoh/src/com/dekkoh/homefeed/PreHomeScreen.java
+++ Dekkoh/src/com/dekkoh/homefeed/PreHomeScreen.java
@@ -2,12 +2,15 @@ package com.dekkoh.homefeed;
 
 import java.util.List;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.dekkoh.R;
 import com.dekkoh.application.BaseActivity;
 import com.dekkoh.datamodel.Question;
+import com.dekkoh.service.APIProcessor;
 
 public class PreHomeScreen extends BaseActivity{
 	
@@ -23,5 +26,37 @@ public class PreHomeScreen extends BaseActivity{
 		tv.setText("Patience is a virtue.");
 		
 	}
+	
+	public class FetchQuestionTask extends AsyncTask<Void, Void, List<Question>>{
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			progressDialogHandler.showCustomProgressDialog(activity);
+		}
+		
+		@Override
+		protected List<Question> doInBackground(Void... params) {
+			try {
+				return APIProcessor.getQuestions(activity, 0, 20, 0, 0, null, null, 0, null, true, null, null, null);			
+				} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+			
+		}
+
+		@Override
+		protected void onPostExecute(List<Question> qlist) {
+			if(qlist!=null){
+				progressDialogHandler.dismissCustomProgressDialog(activity);
+				QuestionContentManager.getInstance().setQuestionList(qlist);
+				Intent intent = new Intent();
+				intent.setClass(activity, HomeScreen.class);
+				startActivity(intent);
+			}
+		}
+	}
+
 
 }
