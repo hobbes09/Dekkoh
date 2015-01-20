77654c6f3f951179ec77186d2a1a020fde255c1c
hobbes09 <sourin.sutradhar@gmail.com>
2015-01-20 16:06:06 +0530

    BugFix - "Location captured with a , to start with" from app issue

--- Dekkoh/src/com/dekkoh/homefeed/QuestionCardView.java
+++ Dekkoh/src/com/dekkoh/homefeed/QuestionCardView.java
@@ -177,7 +177,7 @@ public class QuestionCardView {
     }
 
     private void setValues() {
-        tvLocation.setText(getInstance().question.getLocation());
+        tvLocation.setText(getInstance().question.getLocation().substring(1));
         tvUsername.setText(getInstance().question.getUserName().split(" ", 2)[0]);
         tvQuestion.setText(getInstance().question.getQuestion());
         tvNumAnswers.setText(Integer.toString(getInstance().question
